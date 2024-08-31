50d53f3d2d61c613dff98a8d7b3b00ebed551159
ontofractal <v@ontofractal.com>
2024-09-01 00:09:48 +0300

    minor fixes

--- mix.exs
+++ mix.exs
@@ -1,15 +1,19 @@
-defmodule AIex.MixProject do
+defmodule Ailixir.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :aiex,
+      app: :ailixir,
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
