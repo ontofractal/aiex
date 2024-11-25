f795cb95f7ab48068cc51cb260fe863b82c1498d
ontofractal <v@ontofractal.com>
2024-11-25 22:04:23 +0100

    update formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,11 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    field: 2,
+    field: 3,
+    inputs: 1,
+    output: 1,
+    input_template: 1
+  ]
 ]
